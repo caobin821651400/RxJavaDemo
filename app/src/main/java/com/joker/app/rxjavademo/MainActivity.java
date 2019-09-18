2e898066c382fc59912f9ffbc23e79e326ae21d2
caobin <821651400@qq.com>
2019-09-18 15:28:11 +0800

    first

--- app/src/main/java/com/joker/app/rxjavademo/MainActivity.java
+++ app/src/main/java/com/joker/app/rxjavademo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.joker.app.rxjavademo;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void presss(View view) {
     }
 }
--- app/src/main/java/com/joker/app/rxjavademo/SimpleUseActivity.java
+++ app/src/main/java/com/joker/app/rxjavademo/SimpleUseActivity.java
@@ -1,20 +1,19 @@
 package com.joker.app.rxjavademo;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.joker.app.rxjavademo.utils.XLogUtils;
 
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 /**
  * ====================================================
@@ -29,6 +28,11 @@ public class SimpleUseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void presss(View view) {
+        demo();
     }
 
     //1. 定义
@@ -57,88 +61,79 @@ public class SimpleUseActivity extends AppCompatActivity {
 
 
     /*******==========================================================================**********/
+    public void demo() {
+        //---->步骤1. 创建被观察者Observable
+        //****方式1.
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            //create是RxJava最基本的创建事件序列的方法
+            //当Observable被订阅时，OnSubscribe的Call()方法会自动被调用，即事件会依次触发
+            //即观察者依次调用事件的复写方法从而响应事件
+            //从而实现被观察者调用观察者的回调方法 & 由被观察者想观察者事件传递即观察者模式
 
-    //---->步骤1. 创建被观察者Observable
-    //****方式1.
-    Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
-        //create是RxJava最基本的创建事件序列的方法
-        //当Observable被订阅时，OnSubscribe的Call()方法会自动被调用，即事件会依次触发
-        //即观察者依次调用事件的复写方法从而响应事件
-        //从而实现被观察者调用观察者的回调方法 & 由被观察者想观察者事件传递即观察者模式
+            //从写Subscribe定义需要发送的事件
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                //通过ObservableEmitter类产生事件并通知观察者
+                emitter.onNext(1);
+                emitter.onNext(2);
+//                emitter.onError(new RuntimeException("异常"));
+                emitter.onNext(3);
+                emitter.onComplete();
+            }
+        });
 
-        //从写Subscribe定义需要发送的事件
-        @Override
-        public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
-            //通过ObservableEmitter类产生事件并通知观察者
-            emitter.onNext(1);
-            emitter.onNext(2);
-            emitter.onNext(3);
-            emitter.onComplete();
-        }
-    });
-
-    //****方式2. 创建方式类似上面
-    Observable observable2 = Observable.just(1, 2, 3);
+        //****方式2. 创建方式类似上面
+        Observable<Integer> observable2 = Observable.just(1, 2, 3);
 
 
-    //---->步骤2. 创建观察者
+        //---->步骤2. 创建观察者
 
-    //****方式1. 采用Observer接口
-    Observer<Integer> observer = new Observer<Integer>() {
+        //****方式1. 采用Observer接口
+        Observer<Integer> observer = new Observer<Integer>() {
 
-        //观察者接收事件前，最先调用onSubscribe
-        @Override
-        public void onSubscribe(Disposable d) {
-            XLogUtils.d("开始采用subscribe连接");
-        }
+            //观察者接收事件前，最先调用onSubscribe
+            @Override
+            public void onSubscribe(Disposable d) {
+                XLogUtils.d("开始采用subscribe连接");
+            }
 
-        //当被观察者产生next事件&观察者接收到事件时回调此方法
-        @Override
-        public void onNext(Integer integer) {
-            XLogUtils.d("我收到事件--> " + integer.toString());
-        }
+            //当被观察者产生next事件&观察者接收到事件时回调此方法
+            @Override
+            public void onNext(Integer integer) {
+                XLogUtils.d("我收到事件--> " + integer.toString());
+            }
 
-        //当被观察者产生Error事件&观察者接收到事件时回调此方法
-        @Override
-        public void onError(Throwable e) {
-            XLogUtils.d("收到Error事件");
-        }
+            //当被观察者产生Error事件&观察者接收到事件时回调此方法
+            @Override
+            public void onError(Throwable e) {
+                XLogUtils.d("收到Error事件-> " + e.getLocalizedMessage());
+            }
 
-        //当被观察者产生Complete事件&观察者接收到事件时回调此方法
-        @Override
-        public void onComplete() {
-            XLogUtils.d("收到Complete事件");
-        }
-    };
+            //当被观察者产生Complete事件&观察者接收到事件时回调此方法
+            @Override
+            public void onComplete() {
+                XLogUtils.d("收到Complete事件");
+            }
+        };
 
-    //****方式2. 采用Subscribe抽象类
-    Subscriber<Integer> subscriber = new Subscriber<Integer>() {
-        @Override
-        public void onSubscribe(Subscription s) {
-            XLogUtils.d("开始采用subscribe连接");
-        }
 
-        @Override
-        public void onNext(Integer integer) {
-            XLogUtils.d("我收到事件--> " + integer.toString());
-        }
+        //$$$$$$$$$$$两种方式的区别
+        //相同点：两者使用方式完全一样，实际上在RxJava的subscribe过程中，Observer会被转换成Subscriber再使用
+        //不同点：Subscriber抽象类对Observer进行扩展，新增了两个方法
+        //      1.onStart(): 在未响应事件之前调用
+        //      2.unSubscribe():用于取消订阅。改方法调用后，观察者不在接收&响应事件。
+        //调用此方法前应先使用isUnSubscribed()判断被观察者是否持有观察者的引用，防止内存泄漏。
 
-        @Override
-        public void onError(Throwable t) {
-            XLogUtils.d("收到Error事件");
-        }
 
-        @Override
-        public void onComplete() {
-            XLogUtils.d("收到Complete事件");
-        }
-    };
+        //---->步骤3. 通过订阅连接观察者和被观察者
+        // observable2.subscribe(observer);
 
-    //$$$$$$$$$$$两种方式的区别
-    //相同点：两者使用方式完全一样，实际上在RxJava的subscribe过程中，Observer会被转换成Subscriber再使用
-    //不同点：Subscriber抽象类对Observer进行扩展，新增了两个方法
-    //      1.onStart(): 在未响应事件之前调用
-    //      2.unSubscribe():用于取消订阅。改方法调用后，观察者不在接收&响应事件。
-    //调用此方法前应先使用isUnSubscribed()判断被观察者是否持有观察者的引用，防止内存泄漏。
+        //$$$$$$$$$$$优雅链式调用
+        Observable.just(1, 2, 3).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
 
+            }
+        });
+    }
 }
