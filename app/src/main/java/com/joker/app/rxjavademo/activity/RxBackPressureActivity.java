e46e986d2414a8a453825b2b5d28ba5e8a8dc9b7
caobin <821651400@qq.com>
2019-09-29 14:55:32 +0800

    背压

--- app/src/main/java/com/joker/app/rxjavademo/activity/RxBackPressureActivity.java
+++ app/src/main/java/com/joker/app/rxjavademo/activity/RxBackPressureActivity.java
@@ -2,6 +2,7 @@ package com.joker.app.rxjavademo.activity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import com.joker.app.rxjavademo.R;
@@ -10,6 +11,8 @@ import com.joker.app.rxjavademo.utils.XLogUtils;
 
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
@@ -79,90 +82,90 @@ public class RxBackPressureActivity extends BaseActivity {
 
 
         //2. -->背压简单实用
-//        Flowable.create(new FlowableOnSubscribe<Integer>() {
-//            @Override
-//            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
-//                for (int i = 0; i < 4; i++) {
-//                    emitter.onNext(i);
-//                }
-//            }
-//        }, BackpressureStrategy.ERROR).subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Subscriber<Integer>() {
-//                    @Override
-//                    public void onSubscribe(Subscription s) {
-//                        //与Disposable类似
-//                        //不同点则是多了个request(int count)方法;
-//                        //代表依次接收多少个事件，多余的事件放到缓冲区，
-//                        ss = s;
-//                        ss.request(2);
-//                    }
-//
-//                    @Override
-//                    public void onNext(Integer integer) {
-//                        try {
-//                            XLogUtils.d("接收事件-> " + integer);
-//                        } catch (Exception e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable t) {
-//                        XLogUtils.d("错误 -" + t);
-//                    }
-//
-//                    @Override
-//                    public void onComplete() {
-//
-//                    }
-//                });
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                //观察者去缓存区拿下2条事件
-//                ss.request(2);
-//            }
-//        }, 2000);
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                for (int i = 0; i < 4; i++) {
+                    emitter.onNext(i);
+                }
+            }
+        }, BackpressureStrategy.ERROR).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        //与Disposable类似
+                        //不同点则是多了个request(int count)方法;
+                        //代表依次接收多少个事件，多余的事件放到缓冲区，
+                        ss = s;
+                        ss.request(2);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        try {
+                            XLogUtils.d("接收事件-> " + integer);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        XLogUtils.d("错误 -" + t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //观察者去缓存区拿下2条事件
+                ss.request(2);
+            }
+        }, 2000);
         //$$$$$$$注意点：
         //          A:s.request(3);但是被观察者依然可以发送事件，只不过事件是放在缓存区中。
         //          B:缓存区大小为128；超出128则会报错
 
 
         //3. -->同步调用,被观察者调用requested()去知道观察者一次接收几个事件
-        Flowable.create(new FlowableOnSubscribe<Integer>() {
-            @Override
-            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
-                //这里的数量就是onSubscribe中返回的数量
-                //==0时代表观察者不接受事件，如果在发送则会抛异常
-                //特性：实时更新
-                //     可以叠加:就是在onSubscribe调两次request(2) request(2) 等同于调了request(4)
-                long count = emitter.requested();
-                for (int i = 0; i < count; i++) {
-                    emitter.onNext(i);
-                }
-            }
-        }, BackpressureStrategy.ERROR).subscribe(new Subscriber<Integer>() {
-            @Override
-            public void onSubscribe(Subscription s) {
-                s.request(2);
-            }
-
-            @Override
-            public void onNext(Integer integer) {
-                XLogUtils.i("用多少发多少-> " + integer);
-            }
-
-            @Override
-            public void onError(Throwable t) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
+//        Flowable.create(new FlowableOnSubscribe<Integer>() {
+//            @Override
+//            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+//                //这里的数量就是onSubscribe中返回的数量
+//                //==0时代表观察者不接受事件，如果在发送则会抛异常
+//                //特性：实时更新
+//                //     可以叠加:就是在onSubscribe调两次request(2) request(2) 等同于调了request(4)
+//                long count = emitter.requested();
+//                for (int i = 0; i < count; i++) {
+//                    emitter.onNext(i);
+//                }
+//            }
+//        }, BackpressureStrategy.ERROR).subscribe(new Subscriber<Integer>() {
+//            @Override
+//            public void onSubscribe(Subscription s) {
+//                s.request(2);
+//            }
+//
+//            @Override
+//            public void onNext(Integer integer) {
+//                XLogUtils.i("用多少发多少-> " + integer);
+//            }
+//
+//            @Override
+//            public void onError(Throwable t) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        });
     }
 
     /**
@@ -181,15 +184,16 @@ public class RxBackPressureActivity extends BaseActivity {
      * @param view
      */
     @SuppressLint("CheckResult")
-    private void synchronizeFun(View view) {
+    public void synchronizeFun(View view) {
         // 观察者消费事件速度慢，因为是同步的，所有只有当onNext(1)执行后，一直阻塞到观察者处理完被观察者才会执行onNext(2)
         //被观察者和观察者在同一个线程中
         Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
-                emitter.onNext(1);
-                emitter.onNext(2);
-                emitter.onNext(3);
+                for (int i = 0; i < 3; i++) {
+                    XLogUtils.d("发送事件 " + (i + 1));
+                    emitter.onNext(i + 1);
+                }
             }
         }).subscribe(new Consumer<Integer>() {
             @Override
@@ -242,5 +246,50 @@ public class RxBackPressureActivity extends BaseActivity {
         //2019-09-25 10:33:48.419 I/RxJava: 异步-> 0
         //2019-09-25 10:33:50.421 I/RxJava: 异步-> 1
         //2019-09-25 10:33:52.426 I/RxJava: 异步-> 2
+
+    }
+
+
+    //$$$$$$$$$$$$$$-->背压模式类型
+    //                            A:BackpressureStrategy.ERROR：超出128就会抛异常
+    //                            B:BackpressureStrategy.BUFFER:设置缓存区无限大
+    //                            C:BackpressureStrategy.MISSING：友好提示缓存区满了
+    //                            D:BackpressureStrategy.DROP：超出128的事件丢弃
+    //                            E:BackpressureStrategy.LATEST：只保存最新的事件，超出128的丢弃
+
+
+    @SuppressLint("CheckResult")
+    public void other(View view) {
+        Flowable.interval(1, TimeUnit.MILLISECONDS)
+                .onBackpressureBuffer()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Long>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        s.request(Long.MAX_VALUE);
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        try {
+                            Thread.sleep(500);
+                            XLogUtils.v("其他-> " + aLong);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
     }
 }
